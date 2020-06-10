01bf38a70a55696dffa8e4b6062c29dd4fd2093b
4497cv <ie707560@iteso.mx>
2020-06-09 23:31:33 -0600

    Commented code

--- pong.py
+++ pong.py
@@ -1,36 +1,45 @@
+#  \file:    pong.py
+#  \author:  @4497cv
+#  \date:    June 8th 2020
+#  \brief:   Python Game: Pong
+
+# libraries
 import turtle
 import time
 
-window = turtle.Screen()
-window.title("Pong game")
-window.bgcolor("black")
-window.setup(width=800, height=600)
-window.tracer(0)
+# Window
+window = turtle.Screen()                            # create window object
+window.title("Pong game")                           # assign window's title
+window.bgcolor("black")                             # assign window's background color
+window.setup(width=800, height=600)                 # set window's dimensions
+window.tracer(0)                                    # turn off drawing animation
 
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(-350,0)
+# Paddle for player A
+paddle_a = turtle.Turtle()                          # create paddle_a object
+paddle_a.speed(0)                                   # set paddle's drawing speed to be the fastest
+paddle_a.shape("square")                            # set paddle's shape as a square
+paddle_a.color("white")                             # set paddle's color to be white
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)    # set paddle's shape size
+paddle_a.penup()                                    # no drawing when moving
+paddle_a.goto(-350,0)                               # move paddle to coordinates(-350,0)
 
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
-paddle_b.goto(350,0)
+# Paddle for player B
+paddle_b = turtle.Turtle()                          # create paddle_b object
+paddle_b.speed(0)                                   # set paddle's drawing speed to be the fastest
+paddle_b.shape("square")                            # set paddle's shape as a square
+paddle_b.color("white")                             # set paddle's color to be white
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)    # set paddle's shape size
+paddle_b.penup()                                    # no drawing when moving
+paddle_b.goto(350,0)                                # move paddle to coordinates(350,0)
 
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0,0)
-ball.dx = -2
-ball.dy = 2
+ball = turtle.Turtle()                              # create ball object
+ball.speed(0)                                       # set ball's drawing speed to be the fastest
+ball.shape("square")                                # set ball's shape as a square    
+ball.color("white")                                 # set ball's color to be white
+ball.penup()                                        # no drawing when moving
+ball.goto(0,0)                                      # move ball to the center of the screen
+ball.dx = -2                                        # set dx value (horizontal change)
+ball.dy = 2                                         # set dy value (vertical change)
 
 def paddle_a_up():
     y = paddle_a.ycor()
